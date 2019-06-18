2e52877a2083e06d3533bb85ac01652a89f252b7
mariethbn <51126802+mariethbn@users.noreply.github.com>
2019-06-18 16:18:00 +0200

    Etape 4

--- 2/script.js
+++ 2/script.js
@@ -2,6 +2,12 @@
 $( document ).ready(function() {
 	alert("Bienvenue");
 	$( "p" ).on( "click", function() {
-          $("p").addClass('red');
-	});
-})
+   
+if($(this).hasClass ('red')){
+	$(this).removeClass ('red');
+}else{
+	$(this).addClass('red');
+}
+$(this).html("Get ready!")
+});
+});
