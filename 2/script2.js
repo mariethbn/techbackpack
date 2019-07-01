516f6fba0fd9e0ee996c98cf7db182a4c4d7a5c7
mariethbn <51126802+mariethbn@users.noreply.github.com>
2019-07-01 17:00:35 +0200

    Etape Personnalisation

--- 2/script2.js
+++ 2/script2.js
@@ -1,3 +1,15 @@
 $( document ).ready(function() {
   alert ("Hello");
 })
+function getUsername() {
+    //affiche une boite de dialogue 
+    var  user = prompt("Veuillez rentrez votre nom:", "");
+    // la fonction se termine en retournant le nom récupéré par la boite de dialogue
+    return user
+}
+$(document).ready (function welcome() {
+    var name= getUsername();
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+
+});
+// Appel la fonction welcome();
