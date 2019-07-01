d2d12fe2098c383167a78d51f1d0030163178772
mariethbn <51126802+mariethbn@users.noreply.github.com>
2019-07-01 17:14:18 +0200

    Appel serveur

--- 2/script2.js
+++ 2/script2.js
@@ -13,3 +13,37 @@ $(document).ready (function welcome() {
 
 });
 // Appel la fonction welcome();
+
+function loadChallenges() {
+  $.ajax(
+        {url : 'https://s3.eu-central-1.amazonaws.com/spintransfer/challenge.json',
+        type: 'GET',
+        dataType: 'json'} 
+  )
+  .done(function(data) {
+    showChallenge(data);
+  })
+  .fail(function() {
+    alert( "error" );
+  });
+}
+
+function showChallenge(liste) {
+
+  var data="";
+
+  for ( var i=0; i < liste.length; i++) {
+    data+='<div class="defi">';
+
+    var challenge=liste[i];
+
+    data+='<h2>'+challenge.nom+'</h2>';
+    data+='<p>'+challenge.description+'</p>';
+    data+='<iframe src="'+challenge.youtube+'" allowfullscreen="" width="364" height="204" frameborder="0"></iframe>'
+    data+="</div>";
+  }
+  $("#defis").html(data);
+
+}
+
+loadChallenges();
